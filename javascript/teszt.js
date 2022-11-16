00c4bcae2abc20223c125b3604014b544e32e9c2
O48WRX <kardyy81@std.uni-eszterhazy.hu>
2022-11-16 14:43:27 +0100

    Added old HMTL and Javascript files

---
+++ javascript/teszt.js
@@ -0,0 +1,16 @@
+
+function PrintFromScript() {
+    document.getElementById("nameReturn").textContent = document.getElementById("name").value;
+    document.getElementById("passReturn").textContent = document.getElementById("password").value;
+    document.getElementById("ageReturn").textContent = document.getElementById("age").value;
+}
+
+function assessCalcs() {
+    var a, b ,c ,d;
+    a = document.getElementById("num1").value;
+    b = document.getElementById("num2").value;
+    c = document.getElementById("num3").value;
+    d = document.getElementById("num4").value;
+    document.getElementById("additionResult").textContent = parseInt(a) + parseInt(b);
+    document.getElementById("subtractionResult").textContent = parseInt(c) - parseInt(d);
+}
